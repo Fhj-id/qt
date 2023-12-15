48047e0d1c64bcbaa5840301d5958309ba5b629a
Feng Haojie <873663920@qq.com>
2023-12-15 15:25:00 +0800

    第一次

---
+++ test.py
@@ -0,0 +1,4 @@
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton, QAction, QLineEdit, QMessageBox, QLabel
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
+import sys
